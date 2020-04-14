a9110ea7d8553e94844d508982bcfa239fe49b47
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-04-13 21:57:14 -0400

    added Speaker model and controller

---
+++ ConferenceApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConferenceApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+
+        }
+
+        public DbSet<Speaker>Speakers { get; set; }
+    }
+}
